da685c31464ef366d0522ac42b382ea4b72183dd
georgiome <40315638+megerundi@users.noreply.github.com>
2025-01-29 01:26:16 +0300

    update activate subs function

--- src/commands/activatePremium.js
+++ src/commands/activatePremium.js
@@ -1,9 +1,13 @@
 import { updateUserSubscription} from "../services/userService.js";
 
 export default async (ctx) =>{
-    const telegramId = ctx.from.id;
-    const subscriptionEndDate = new Date();
-    subscriptionEndDate.setFullYear(subscriptionEndDate.getFullYear() + 10);
-
-    await updateUserSubscription(telegramId, true, subscriptionEndDate);
+    try{
+        const telegramId = ctx.from.id;
+        const subscriptionEndDate = new Date();
+        subscriptionEndDate.setFullYear(subscriptionEndDate.getFullYear() + 10);
+        await updateUserSubscription(telegramId, true, subscriptionEndDate);
+        await ctx.reply('Подписка активирована на 10 лет!');
+    } catch (error){
+        console.log('Ошибка активации подписки', error);
+    }
 }
